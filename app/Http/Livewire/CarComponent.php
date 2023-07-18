4ad1bf0cd2e756dae974dad194717f212848a346
boniich <ezequieldbo25@gmail.com>
2023-07-18 18:25:31 -0300

    feat: add the list of product's car and option to delete it

--- app/Http/Livewire/CarComponent.php
+++ app/Http/Livewire/CarComponent.php
@@ -5,18 +5,43 @@ namespace App\Http\Livewire;
 use App\Models\User;
 use Livewire\Component;
 
+
 class CarComponent extends Component
 {
+
+    public $check = false;
+    public $newData;
+    public $data;
+
+    protected $listeners = [
+        'getCar' => 'getCar'
+    ];
+
+    public function mount()
+    {
+        $this->getCar();
+    }
+
+    public function getCar()
+    {
+        $this->data = User::find(Auth()->user()->id);
+        $this->newData = $this->data->products;
+    }
+
     public function render()
     {
-        $data = User::find(Auth()->user()->id);
+        if ($this->check) {
+            dd($this->newData);
+        }
+        return view('livewire.car-component', ['newData' => $this->newData]);
+    }
 
-        $newData = $data->products;
-
-        // dd($newData);
-
-        // dd($data->products[0]->name);
-
-        return view('livewire.car-component', compact('newData'));
+    public function remove($id)
+    {
+        $this->data->products()->detach($id);
+        $this->data = User::find(Auth()->user()->id);
+        $this->newData = $this->data->products;
+        // $this->emit('getCar');
+        $this->check = !true;
     }
 }
