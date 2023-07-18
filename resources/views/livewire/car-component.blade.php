1ec36d9ca1aed8936bf2e4cc42e492d4ec819731
boniich <ezequieldbo25@gmail.com>
2023-07-18 18:38:31 -0300

    feat: add msg for when there is not products in car

--- resources/views/livewire/car-component.blade.php
+++ resources/views/livewire/car-component.blade.php
@@ -7,43 +7,51 @@
 
             <div class="mt-8">
                 <ul class="h-96 space-y-4 bg-white p-3.5 overflow-y-scroll">
-                    @foreach ($newData as $oneData)
-                        <li class="flex items-center gap-4 hover:bg-gray-100">
-                            <img src={{ 'storage/' . $oneData->image }} alt=""
-                                class="h-24 w-24 rounded object-cover" />
+                    @if (sizeof($newData) > 0)
+                        @foreach ($newData as $oneData)
+                            <li class="flex items-center gap-4 hover:bg-gray-100">
+                                <img src={{ 'storage/' . $oneData->image }} alt=""
+                                    class="h-24 w-24 rounded object-cover" />
 
-                            <div>
-                                <h1 class="text-lg text-black-900">{{ $oneData->name }}</h1>
+                                <div>
+                                    <h1 class="text-lg text-black-900">{{ $oneData->name }}</h1>
 
-                                <div class="mt-0.5 space-y-px text-[10px] text-gray-600">
-                                    <div>
-                                        <p class="">{{ $oneData->short_description }}</p>
-                                        <strong class="text-sm">${{ $oneData->price }}</strong>
+                                    <div class="mt-0.5 space-y-px text-[10px] text-gray-600">
+                                        <div>
+                                            <p class="">{{ $oneData->short_description }}</p>
+                                            <strong class="text-sm">${{ $oneData->price }}</strong>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
 
-                            <div class="flex flex-1 items-center justify-end gap-2">
-                                <form>
-                                    <label for="Line3Qty" class="sr-only"> Quantity </label>
+                                <div class="flex flex-1 items-center justify-end gap-2">
+                                    <form>
+                                        <label for="Line3Qty" class="sr-only"> Quantity </label>
 
-                                    <input type="number" min="1" value="1" id="Line3Qty"
-                                        class="h-8 w-12 rounded border-gray-200 bg-gray-50 p-0 text-center text-xs text-gray-600 [-moz-appearance:_textfield] focus:outline-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none" />
-                                </form>
+                                        <input type="number" min="1" value="1" id="Line3Qty"
+                                            class="h-8 w-12 rounded border-gray-200 bg-gray-50 p-0 text-center text-xs text-gray-600 [-moz-appearance:_textfield] focus:outline-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none" />
+                                    </form>
 
-                                <button wire:click="remove({{ $oneData->id }})"
-                                    class="text-gray-600 transition hover:text-red-600">
-                                    <span class="sr-only">Remove item</span>
+                                    <button wire:click="remove({{ $oneData->id }})"
+                                        class="text-gray-600 transition hover:text-red-600">
+                                        <span class="sr-only">Remove item</span>
 
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                        stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                                    </svg>
-                                </button>
-                            </div>
-                        </li>
-                    @endforeach
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                        </svg>
+                                    </button>
+                                </div>
+                            </li>
+                        @endforeach
+                    @else
+                        <div class="flex justify-center items-center  mt-44 bg-red-300">
+                            <p class="text-lg">No tienes productos en tu carrito. Compra Algo!</p>
+                        </div>
+
+                    @endif
+
                 </ul>
 
                 <div class="mt-8 flex justify-end border-t border-gray-100 pt-8 bg-white p-3.5">
